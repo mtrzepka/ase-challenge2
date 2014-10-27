8f2925ce5dd9b977475de70c762f5705f1d23001
hgzq4 <me.haritha91@gmail.com>
2014-10-26 23:41:02 -0500

    Api
    GoogleMaps

---
+++ AndroidChallengeTwoFinal/ChallengeTwoHiveLast/src/com/example/challengetwohivelast/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.challengetwohivelast;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+
+
+import android.widget.Toast;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends Activity {
+
+	// Google Map
+    private GoogleMap googleMap;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+                
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+ 
+}
