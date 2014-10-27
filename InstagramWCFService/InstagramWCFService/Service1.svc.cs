1e7968fb7738e33fbfb0471721ef0349d5a29cda
Michael Rzepka <mike.rzepka@cerner.com>
2014-10-26 22:27:10 -0500

    Add Instagram WCF Custom REST Service

---
+++ InstagramWCFService/InstagramWCFService/Service1.svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace InstagramWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "data/{value}")]
+        public ImageExists GetData(String value)
+        {
+            bool found = false;
+            //Declare Connection by passing the connection string from the web config file
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbString"].ConnectionString);
+
+            //Open the connection
+            conn.Open();
+
+            //Declare the sql command
+            SqlCommand cmd = new SqlCommand("Select count(*) From Seen where id='" + value + "'", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.GetInt32(0) > 0)
+                    found = true;
+            }
+            reader.Close();
+
+            if (found == false)
+            {
+                cmd = new SqlCommand("Insert into Seen(id)values('" + value + "')", conn);
+
+                //Execute the insert query
+                cmd.ExecuteNonQuery();
+            }
+
+            cmd.Dispose();
+            //close the connection
+            conn.Close();
+
+            return new ImageExists
+                {
+                    exists = found
+                };
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
